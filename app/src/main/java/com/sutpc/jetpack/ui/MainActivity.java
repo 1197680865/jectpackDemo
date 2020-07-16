2150a5fe65f7eefbfae194f4c898f55a24b962e1
zhangchen <1197680876@qq.com>
2020-07-16 20:30:15 +0800

    初始提交

---
+++ app/src/main/java/com/sutpc/jetpack/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sutpc.jetpack.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sutpc.jetpack.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
