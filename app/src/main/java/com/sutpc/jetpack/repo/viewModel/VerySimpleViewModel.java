a26633ab684c2cce4b5275992191a2ba68e8b6a2
zhangchen <1197680876@qq.com>
2020-07-16 21:13:33 +0800

    最简单的ViewModel的使用

---
+++ app/src/main/java/com/sutpc/jetpack/repo/viewModel/VerySimpleViewModel.java
@@ -0,0 +1,14 @@
+package com.sutpc.jetpack.repo.viewModel;
+
+import androidx.lifecycle.ViewModel;
+
+
+/**
+ *  @Description: 最简单的 viewModel
+ *  屏幕旋转也可保存数据
+ *  @author: zhangchen
+ *  @Date: 2020/7/16 20:46
+ */
+public class VerySimpleViewModel extends ViewModel {
+    public int num = 0;
+}
--- app/src/main/java/com/sutpc/jetpack/ui/MainActivity.java
+++ app/src/main/java/com/sutpc/jetpack/ui/MainActivity.java
@@ -1,16 +1,42 @@
 package com.sutpc.jetpack.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.sutpc.jetpack.R;
+import com.sutpc.jetpack.repo.viewModel.VerySimpleViewModel;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.textView)
+    TextView textView;
+    @BindView(R.id.button)
+    Button button;
+    private VerySimpleViewModel verySimpleViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        testVerySimpleVM();
+    }
+
+    /** *
+     * 测试最简单的VM，屏幕旋转时数据可保存
+     */
+    private void testVerySimpleVM(){
+        //获取VerySimpleViewModel 需要引入  'androidx.lifecycle:lifecycle-extensions:2.2.0'
+        verySimpleViewModel = new ViewModelProvider(this).get(VerySimpleViewModel.class);
+        button.setOnClickListener(view -> {
+            verySimpleViewModel.num ++ ;
+            textView.setText( "测试最简单的VM：" + verySimpleViewModel.num);
+        });
     }
 }
